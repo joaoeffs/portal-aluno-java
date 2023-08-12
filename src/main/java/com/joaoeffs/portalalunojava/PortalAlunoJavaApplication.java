aa7294485b6f5ca42076d4ec4e5875291512e1c4
joao.effting <joao.effting@totvs.com.br>
2023-08-12 11:16:07 -0300

    Start

---
+++ src/main/java/com/joaoeffs/portalalunojava/PortalAlunoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joaoeffs.portalalunojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAlunoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAlunoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoeffs/portalalunojava/PortalAlunoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoeffs.portalalunojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAlunoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
