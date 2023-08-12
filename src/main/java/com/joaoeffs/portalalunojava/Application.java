f15d95b95c688543c214b8aaf30374cd350e4268
joao.effting <joao.effting@totvs.com.br>
2023-08-12 14:05:16 -0300

    Conectando ao banco de dados local

--- src/main/java/com/joaoeffs/portalalunojava/Application.java
+++ src/main/java/com/joaoeffs/portalalunojava/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortalAlunoJavaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortalAlunoJavaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
