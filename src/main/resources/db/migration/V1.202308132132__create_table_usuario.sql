6f214a210148d55cd7f3af7f7b02c80c0d8ad453
joao.effting <joao.effting@totvs.com.br>
2023-09-07 18:11:20 -0300

    Removendo toda a parte do registro do usuario

--- src/main/resources/db/migration/V1.202308132132__create_table_usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuario (
-    id          uuid               PRIMARY KEY UNIQUE NOT NULL,
-    login       VARCHAR(64)        UNIQUE NOT NULL,
-    senha       VARCHAR(64)        NOT NULL,
-    role        VARCHAR(64)
-);
