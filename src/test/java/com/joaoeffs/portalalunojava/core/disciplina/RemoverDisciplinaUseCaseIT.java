71c60ea43e167f7129ea3deaada82938d455e7c9
joao.effting <joao.effting@totvs.com.br>
2023-11-08 15:21:22 -0300

    TESTE-01 - Alterar e Remover Disciplina

---
+++ src/test/java/com/joaoeffs/portalalunojava/core/disciplina/RemoverDisciplinaUseCaseIT.java
@@ -0,0 +1,55 @@
+package com.joaoeffs.portalalunojava.core.disciplina;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joaoeffs.portalalunojava.core.domain.disciplina.model.Disciplina;
+import com.joaoeffs.portalalunojava.core.domain.disciplina.repository.DisciplinaDomainRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class RemoverDisciplinaUseCaseIT {
+
+    @Autowired
+    private DisciplinaDomainRepository repository;
+
+    @Autowired
+    MockMvc mock;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    private Disciplina disciplina;
+
+    @BeforeEach
+    public void setup() {
+        disciplina = Disciplina.builder()
+            .nome("Ciência de Dados")
+            .codigo("CIENDD-2023")
+            .build();
+
+        repository.save(disciplina);
+    }
+
+    @Test
+    public void caminhoFeliz() throws Exception {
+
+        UUID id = disciplina.getId();
+
+        mock.perform(delete("/api/disciplina/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+}
