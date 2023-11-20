7f019f2d28245d507b44371b06357769c82a2dd1
joao.effting <joao.effting@totvs.com.br>
2023-11-20 16:39:30 -0300

    PIPE-01 - Pipeline prod

---
+++ src/test/java/com/joaoeffs/portalalunojava/PortalAlunoJavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.joaoeffs.portalalunojava;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PortalAlunoJavaApplicationTests {
+
+    @Test
+     void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
